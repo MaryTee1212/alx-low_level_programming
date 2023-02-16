b8e1262556d35764d67f035b3761362b6f9fdbfe
MaryTee1212 <agadamary1212@gmail.com>
2023-02-16 14:52:45 -0800

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,11 @@
 
 /**
  * main - A program that prints a line with puts function
- * Return - always 0
+ * Return: always 0
  */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
